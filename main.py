9b34487896bbb89e4d7df723d8f98e50cb8e7ec1
MRaehanR <raehanrobban2@gmail.com>
2024-12-11 14:00:27 +0700

    add tabulate package

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from utilities.csv_read_list import *
 from utilities.csv_show_table import *
 
 
 # print(show_csv("/Users/raehan/Kuliah/Semester 1/Algoritma dan Dasar Pemrograman Praktik/tugas/w9/T3_5240411056/transaksi_barang.csv"))
+csv_show_table_tabulate("/Users/raehan/Kuliah/Semester 1/Algoritma dan Dasar Pemrograman Praktik/tugas/w9/T3_5240411056/data_barang.csv")
--- utilities/csv_show_table.py
+++ utilities/csv_show_table.py
@@ -1,5 +1,6 @@
 from utilities.csv_read_list import *
 from utilities.print_with_color import *
+from tabulate import tabulate
 
 def csv_show_table(filepath):
     data = csv_read_list(filepath)
@@ -13,3 +14,11 @@ def csv_show_table(filepath):
 
     for row in data[1:]:
         print(" | ".join(row))
+        
+def csv_show_table_tabulate(filepath):
+    data = csv_read_list(filepath)
+    header = data[0]
+    
+    data.pop(0)
+    
+    print(tabulate(data, headers=header, tablefmt="grid"))
