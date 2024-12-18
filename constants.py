7d5c499fa6424504f418ef76c96dd2208af4b3bf
xoxexy <nadya59440@gmail.com>
2024-12-18 14:49:14 +0700

    Create Profile Feature

--- constants.py
+++ constants.py
@@ -4,5 +4,6 @@ BASE_PATH=os.path.dirname(os.path.abspath(__file__))
 CSV_BASE_PATH=f"{BASE_PATH}/csv"
 USER_CSV=f"{CSV_BASE_PATH}/users.csv"
 CARD_USER_CSV=f"{CSV_BASE_PATH}/card_users.csv"
+CARDS_CSV=f"{CSV_BASE_PATH}/cards.csv"
 CARD_MARKET_CSV=f"{CSV_BASE_PATH}/card_market.csv"
-USER_ID=4
+USER_ID=1
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 from utilities.csv_show_table import *
-from utilities.find_my_cards import *
-from market_features.sell import *
-from market_features.cancel import *
-from market_features.buy import *
+from profile.profil import *
 
-# sell()
-buy()
-
-# print(show_csv("/Users/raehan/Kuliah/Semester 1/Algoritma dan Dasar Pemrograman Praktik/tugas/w9/T3_5240411056/transaksi_barang.csv"))
-csv_show_table_tabulate("/Users/raehan/Kuliah/Semester 1/Algoritma dan Dasar Pemrograman Praktik/tugas/w9/T3_5240411056/data_barang.csv")
+profil()
