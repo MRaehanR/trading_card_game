52e8d9fd8f368cf01c11f62bda3fedae7ce2add6
keylazahra <keylaasyfa17@gmail.com>
2024-12-19 15:12:45 +0700

    register

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from utilities.find_my_cards import *
 from market_features.sell import *
 from market_features.cancel import *
 from market_features.buy import *
+from Register.register import *
 
 # sell()
-buy()
+register()
 
 # print(show_csv("/Users/raehan/Kuliah/Semester 1/Algoritma dan Dasar Pemrograman Praktik/tugas/w9/T3_5240411056/transaksi_barang.csv"))
-csv_show_table_tabulate("/Users/raehan/Kuliah/Semester 1/Algoritma dan Dasar Pemrograman Praktik/tugas/w9/T3_5240411056/data_barang.csv")
+# csv_show_table_tabulate("/Users/raehan/Kuliah/Semester 1/Algoritma dan Dasar Pemrograman Praktik/tugas/w9/T3_5240411056/data_barang.csv")
